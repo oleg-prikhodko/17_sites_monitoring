7f828a048a4084ffedf45136bbdaeed0dc459538
Oleg <super.oleg.3000@yandex.ru>
2018-10-09 12:26:16 +0300

    Add expiration date getter

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,5 +1,8 @@
 from urllib.request import urlopen
 
+import whois
+
+
 def load_urls4check(path):
     pass
 
@@ -8,7 +11,8 @@ def is_server_respond_with_200(url):
     return response.status == 200
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois(domain_name)
+    return domain_info.expiration_date
 
 if __name__ == '__main__':
     pass
