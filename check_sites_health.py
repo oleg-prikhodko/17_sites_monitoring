24f951a7d47e84e6f3bcb982ed175cf2f1587f1b
Oleg <super.oleg.3000@yandex.ru>
2018-10-10 17:32:00 +0300

    Add proper status message

--- check_sites_health.py
+++ check_sites_health.py
@@ -2,10 +2,9 @@ import argparse
 import os.path
 import sys
 from datetime import datetime, timedelta
-from urllib.error import URLError
 from urllib.parse import urlsplit
-from urllib.request import urlopen
 
+import requests
 import whois
 
 
@@ -16,8 +15,8 @@ def load_urls_from_file(path):
 
 
 def has_server_responded_with_200(url):
-    response = urlopen(url)
-    return response.status == 200
+    response = requests.get(url)
+    return response.status_code == 200
 
 
 def get_domain_expiration_date(domain_name):
@@ -61,18 +60,22 @@ if __name__ == "__main__":
     registry_expiration_index = 0
 
     for url in urls:
+        try:
+            status_message = "response {} OK".format(
+                "is" if has_server_responded_with_200(url) else "is not"
+            )
+        except requests.exceptions.RequestException as error:
+            sys.exit(error)
+
         expiration_date = get_domain_expiration_date(get_domain_from_url(url))
+        if expiration_date is None:
+            sys.exit("Could not get expiry date for [{}]".format(url))
+
         expiration_date = (
             expiration_date
             if isinstance(expiration_date, datetime)
             else expiration_date[registry_expiration_index]
         )
-        try:
-            status_message = "response {} OK".format(
-                "is" if has_server_responded_with_200(url) else "is not"
-            )
-        except URLError as error:
-            sys.exit(error)
 
         expiration_date_message = "expiry date is {} than month away".format(
             "more" if is_month_away(expiration_date) else "less"
