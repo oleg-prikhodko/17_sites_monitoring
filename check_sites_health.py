67aeb84584caed12a535880ea379603f1eb6fda9
Oleg <super.oleg.3000@yandex.ru>
2018-10-10 12:17:13 +0300

    Add main part

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from urllib.parse import urlsplit
 from urllib.request import urlopen
 
 import whois
@@ -8,13 +10,32 @@ def load_urls4check(path):
         urls = [line.strip() for line in urls_file]
         return urls
 
+
 def is_server_respond_with_200(url):
     response = urlopen(url)
     return response.status == 200
+
 
 def get_domain_expiration_date(domain_name):
     domain_info = whois.whois(domain_name)
     return domain_info.expiration_date
 
-if __name__ == '__main__':
-    pass
+
+def get_domain_from_url(url):
+    return urlsplit(url).netloc
+
+
+if __name__ == "__main__":
+    urls = load_urls4check("urls.txt")
+    for url in urls:
+        expiration_date = get_domain_expiration_date(get_domain_from_url(url))
+        registry_expiration_index = 0
+        expiration_date = (
+            expiration_date
+            if isinstance(expiration_date, datetime)
+            else expiration_date[registry_expiration_index]
+        )
+        message = "[{}] status ok: {}, expiration date: {}".format(
+            url, is_server_respond_with_200(url), expiration_date
+        )
+        print(message)
